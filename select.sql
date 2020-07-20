d3ee8b6a81f7d50687c1aeeb9fab8d6576855eff
Uduak Essien <acushla4real@gmail.com>
2020-07-20 20:53:34 +0100

    Add select solution for capital using LEN function

--- select.sql
+++ select.sql
@@ -49,3 +49,13 @@ WHERE continent = 'South America';
 
 SELECT name, ROUND(gdp/population, -3) as 'per-capita' FROM world
 WHERE gdp > 1000000000000;
+
+
+-- Greece has capital Athens.
+
+-- Each of the strings 'Greece', and 'Athens' has 6 characters.
+
+-- Show the name and capital where the name and the capital have the same number of characters.
+
+SELECT name, capital, FROM world
+WHERE LEN(name) = LEN(capital);
