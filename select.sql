104aa9f98bdceaad33fd64405a5e4161afea8d69
Uduak Essien <acushla4real@gmail.com>
2020-07-20 18:42:18 +0100

    Add select solution for big area

--- select.sql
+++ select.sql
@@ -3,7 +3,7 @@ SELECT name, gdp/population as 'per capita GDP' FROM world
 WHERE population > 200000000;
 
 
-Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
+-- Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
 SELECT name, population/1000000 FROM world
 WHERE continent = 'South America';
 
@@ -12,6 +12,14 @@ SELECT name, population FROM world
 WHERE name IN ('France', 'Germany', 'Italy')
 
 
-Show the countries which have a name that includes the word 'United'
+-- Show the countries which have a name that includes the word 'United'
 SELECT name FROM world
-WHERE name LIKE '%United%'
+WHERE name LIKE '%United%';
+
+-- Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+
+-- Show the countries that are big by area or big by population. Show name, population and area.
+SELECT name, population, area FROM world
+WHERE area > 3000000 OR population > 250000000;
+
+
