cc2341028cb1856c438bfbc647b0de506ee7ddef
Uduak Essien <acushla4real@gmail.com>
2020-07-20 20:29:47 +0100

    Add select solution for big area and population using XOR

--- select.sql
+++ select.sql
@@ -23,3 +23,13 @@ SELECT name, population, area FROM world
 WHERE area > 3000000 OR population > 250000000;
 
 
+-- Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+
+-- Australia has a big area but a small population, it should be included.
+-- Indonesia has a big population but a small area, it should be included.
+-- China has a big population and big area, it should be excluded.
+-- United Kingdom has a small population and a small area, it should be excluded.
+
+SELECT name, population, area FROM world
+WHERE (area > 3000000 AND population < 250000000) OR 
+(area < 3000000 AND population > 250000000);
