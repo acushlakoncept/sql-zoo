77743a2ded961f4267e6f5dd213533877b3f7008
Uduak Essien <acushla4real@gmail.com>
2020-07-21 11:42:52 +0100

    Add select solution for search strings with apostrophy in it

--- select_nobel.sql
+++ select_nobel.sql
@@ -52,4 +52,15 @@ SELECT yr, subject, winner FROM nobel
  WHERE yr = '1980'AND NOT subject IN ('Chemistry', 'Medicine');
 
 
+-- Show year, subject, and name of people who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+SELECT yr, subject, winner FROM nobel
+ WHERE subject = 'Medicine' AND yr < 1910
+ OR subject = 'Literature' AND yr >= 2004;
+
+
+--  Find all details of the prize won by EUGENE O'NEILL
+SELECT yr, subject, winner FROM nobel
+ WHERE winner LIKE '%EUGENE_%_NEILL%';
+
+
 
