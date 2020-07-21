943ab55413aa20afb6511ccf0f828b35b6a5612c
Uduak Essien <acushla4real@gmail.com>
2020-07-21 14:28:52 +0100

    Add SELECT solutions to demonstrate HAVING functions

--- sum_count.sql
+++ sum_count.sql
@@ -40,3 +40,10 @@ FROM world
    WHERE population >= 10000000
    GROUP BY continent
 
+-- List the continents that have a total population of at least 100 million.
+SELECT continent
+FROM world
+   GROUP BY continent
+   HAVING SUM(population) >= 100000000
+
+
