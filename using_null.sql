67a51efa9a68f8f5fc8a83991836e0a2c4a8079d
Uduak Essien <acushla4real@gmail.com>
2020-07-21 16:35:29 +0100

    Add select solution to demonstrate the case function

--- using_null.sql
+++ using_null.sql
@@ -29,3 +29,32 @@ SELECT teacher.name, COALESCE(dept.name, 'None') as department
  FROM teacher LEFT JOIN dept
            ON (teacher.dept=dept.id)
       
+
+-- Use COUNT to show the number of teachers and the number of mobile phones.
+SELECT COUNT(name) as teachers, COUNT(mobile) as mobile FROM teacher
+
+
+-- Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+SELECT dept.name, COUNT(teacher.name)
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id)
+ GROUP BY dept.name
+
+
+--  Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+SELECT name, CASE 
+               WHEN dept = 1 OR dept = 2 THEN 'Sci'
+               ELSE 'Art'
+             END
+FROM teacher
+
+-- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+SELECT name, CASE 
+               WHEN dept = 1 OR dept = 2 THEN 'Sci'
+               WHEN dept = 3 THEN 'Art'
+               ELSE 'None'
+             END
+FROM teacher
+
+
+
