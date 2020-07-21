df31982e75088bf85557b4f795f6d59e98412b3e
Uduak Essien <acushla4real@gmail.com>
2020-07-21 15:29:56 +0100

    Add SELECT solution to use the DISTINCT function, querying data from two tables

--- join.sql
+++ join.sql
@@ -14,3 +14,34 @@ SELECT id,stadium,team1,team2
 SELECT player,teamid, stadium, mdate
   FROM game JOIN goal ON (id=matchid)
   WHERE goal.teamid = 'GER'
+
+
+-- Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+SELECT team1, team2, player
+  FROM game JOIN goal ON (id=matchid)
+  WHERE player LIKE 'Mario%'
+
+
+-- Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON teamid=id
+ WHERE gtime<=10
+
+
+-- List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
+SELECT mdate, teamname
+  FROM game JOIN eteam ON (team1=eteam.id)
+ WHERE coach = 'Fernando Santos'
+
+
+-- List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'
+SELECT player
+  FROM game JOIN goal ON (game.id=matchid)
+ WHERE stadium = 'National Stadium, Warsaw'
+
+
+-- Instead show the name of all players who scored a goal against Germany.
+SELECT DISTINCT player
+  FROM game JOIN goal ON matchid = id 
+    WHERE (team1='GER' OR team2='GER') AND teamid<>'GER'
+
