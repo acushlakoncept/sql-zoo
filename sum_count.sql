c2ca3e83a53a256655754615ac06cb079e2cc614
Uduak Essien <acushla4real@gmail.com>
2020-07-21 14:24:47 +0100

    Add SELECT solutions to demonstrate GROUP BY functions

--- sum_count.sql
+++ sum_count.sql
@@ -16,3 +16,27 @@ SELECT SUM(gdp)
 FROM world
    WHERE continent = 'Africa'
 
+
+-- How many countries have an area of at least 1000000
+SELECT COUNT(name)
+FROM world
+   WHERE area > 1000000
+
+
+-- What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+SELECT SUM(population)
+FROM world
+   WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+
+-- For each continent show the continent and number of countries.
+SELECT continent, COUNT(name)
+FROM world
+   GROUP BY continent
+
+-- For each continent show the continent and number of countries with populations of at least 10 million.
+SELECT continent, COUNT(name)
+FROM world
+   WHERE population >= 10000000
+   GROUP BY continent
+
