8b1556a39506a1f71fa04133d22712e55f262055
Uduak Essien <acushla4real@gmail.com>
2020-07-21 16:21:41 +0100

    Add select solution to demonstrate the coalesce function

--- using_null.sql
+++ using_null.sql
@@ -19,3 +19,13 @@ SELECT teacher.name, dept.name
  FROM teacher RIGHT JOIN dept
            ON (teacher.dept=dept.id)
 
+
+-- Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
+SELECT name, COALESCE(mobile, '07986 444 2266') as mobile FROM teacher
+
+
+-- Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+SELECT teacher.name, COALESCE(dept.name, 'None') as department
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+      
